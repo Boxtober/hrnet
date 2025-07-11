9daf2597fecd1c45b8bbf06d48941f37800cead8
sabrina <sabrina@verymountain.fr>
2025-07-11 12:55:43 +0200

    config github page

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ const theme = createTheme();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/hrnet">
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <App />
